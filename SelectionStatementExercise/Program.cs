928c82a4c4c5b1469854f4d3ad02517d276690f3
nathan stephens <nstephens1234567@gmail.com>
2022-11-29 09:32:15 -0600

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            do
+            {
+
+                Console.WriteLine("Try to guess my favorite number, it is between 1 and 10.");
+                var r = new Random();
+                var favNumber = r.Next(1, 10);
+                var userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Good guess, but that number is too low.");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Nice try, but that number is too high!");
+                }
+                else
+                {
+                    bool v = userInput == favNumber;
+                    Console.WriteLine("Congratulations are in order, for you have guessed correctly!!");
+                    break;
+                }
+            } 
+            while (true);        
         }
     }
+
 }
